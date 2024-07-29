544dcd8a7a5ea830365cea9c19983862b4b2dce3
seleneledain <ledainselene@gmail.com>
2024-07-29 15:46:26 +0200

    Final script to simulate with PROSAIL and soil spectra

--- simulate_S2_spectra_soil.py
+++ simulate_S2_spectra_soil.py
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 from datetime import datetime
-from rtm_inv.core.lookup_table import generate_lut
+from rtm_inv2.core.lookup_table import LookupTable, generate_lut, simulate_from_lut
 import pickle
 import numpy as np
 import pandas as pd
@@ -48,17 +48,18 @@ def get_logger():
 
 
 def generate_spectra(
-  output_dir: Path,
-  lut_params_dir: Path,
-  rtm_lut_config: Dict[str, Any],
-  traits: List[str]
+    output_dir: Path,
+    lut_params_dir: Path,
+    lut_config: Dict[str, Any],
+    rtm_config: Dict[str, Any],
+    traits: List[str]
   ) -> None:
 
   logger = get_logger()
   
   # run PROSAIL forward runs for the different parametrizations available
   logger.info('Starting PROSAIL runs')
-  lut_params_pheno = lut_params_dir.joinpath('prosail_danner-etal_switzerland.csv') # Only use general params for LUT
+  lut_params_pheno = lut_params_dir.joinpath('prosail_danner-etal_switzerland_nosoil.csv') # Only use general params for LUT
 
   pheno_phases = \
       lut_params_pheno.name.split('.csv')[0] +'_S2A'
@@ -67,13 +68,86 @@ def generate_spectra(
   # generate lookup-table
   trait_str = '-'.join(traits)
   fpath_lut = output_dir.joinpath(
-    f'{pheno_phases}_{trait_str}_lut_no-constraints.pkl')
+    f'{pheno_phases}_{trait_str}_lut_no-constraints.pkl') 
 
   # if LUT exists, continue, else generate it
   if not fpath_lut.exists():
-    lut_inp = rtm_lut_config.copy()
+    lut_inp = lut_config.copy()
     lut_inp['lut_params'] = lut_params_pheno
-    lut = generate_lut(**lut_inp) # TO DO: need to modify function so that it simulates for a range of angles
+    lut = generate_lut(**lut_inp)
+    lut = simulate_from_lut(lut, **rtm_config)
+
+    # special case CCC (Canopy Chlorophyll Content) ->
+    # this is not a direct RTM output
+    if 'ccc' in traits:
+      lut['ccc'] = lut['lai'] * lut['cab']
+      # convert to g m-2 as this is the more common unit
+      # ug -> g: factor 1e-6; cm2 -> m2: factor 1e-4
+      lut['ccc'] *= 1e-2
+
+
+    lut.dropna(inplace=True)
+
+    # save LUT to file
+    with open(fpath_lut, 'wb') as f:
+      pickle.dump(lut, f, protocol=3)
+
+  else:
+    pass
+
+  logger.info('Finished PROSAIL runs')
+
+
+def generate_spectra_soil(
+  output_dir: Path,
+  lut_params_dir: Path,
+  lut_config: Dict[str, Any],
+  rtm_config: Dict[str, Any],
+  traits: List[str],
+  soil_df: pd.DataFrame
+  ) -> None:
+
+  logger = get_logger()
+  
+  # run PROSAIL forward runs for the different parametrizations available
+  logger.info('Starting PROSAIL runs')
+  lut_params_pheno = lut_params_dir.joinpath('prosail_danner-etal_switzerland_soil.csv') # Only use general params for LUT
+
+  pheno_phases = \
+      lut_params_pheno.name.split('.csv')[0] +'_S2B'
+      #lut_params_pheno.name.split('etal')[-1].split('.')[0][1::]
+
+  # generate lookup-table
+  trait_str = '-'.join(traits)
+  fpath_lut = output_dir.joinpath(
+    f'{pheno_phases}_{trait_str}_lut_no-constraints.pkl') #
+
+  # if LUT exists, continue, else generate it
+  if not fpath_lut.exists():
+    # Generate LUT
+    lut_inp = lut_config.copy()
+    lut_inp['lut_params'] = lut_params_pheno
+    lut = generate_lut(**lut_inp)
+
+    # Simulate with RTM
+    rtm_inp = rtm_config.copy()
+    # Create LUT subgroups of size lut_size/len(soil_df). 
+    # Pass each subgroup with a soil spectra and simulate.
+    # Concatenate all simulations
+    sub_luts = get_random_subgroups(lut._samples, len(soil_df))
+    lut_allsoils = []
+    for i, (original_idx, sub_lut) in enumerate(sub_luts):
+      logger.info(f'Simulating with soil spectra {i+1} of {len(soil_df)}')
+      rtm_inp['rsoil0'] = None
+      rtm_inp['soil_spectrum1'] = soil_df.iloc[i].values 
+      rtm_inp['soil_spectrum2'] = np.zeros_like(soil_df.iloc[i].values) 
+      sub_lut = dataframe_to_lookup_table(sub_lut, lut)
+      lut_soilspectra = simulate_from_lut(sub_lut, **rtm_config)
+      lut_soilspectra.index = original_idx # keep original order
+      lut_allsoils.append(lut_soilspectra)
+    
+    lut = pd.concat(lut_allsoils).sort_index()
+
 
     # special case CCC (Canopy Chlorophyll Content) ->
     # this is not a direct RTM output
@@ -87,9 +161,10 @@ def generate_spectra(
     # lut = lut[band_selection + traits].copy()
     lut.dropna(inplace=True)
 
+ 
     # save LUT to file
-    with open(fpath_lut, 'wb') as f:
-      pickle.dump(lut, f, protocol=3)
+    with open(fpath_lut, 'wb+') as f:
+      pickle.dump(lut, f)
 
   else:
     pass
@@ -98,75 +173,30 @@ def generate_spectra(
 
 
 
-def generate_spectra_soil(
-  output_dir: Path,
-  lut_params_dir: Path,
-  rtm_lut_config: Dict[str, Any],
-  traits: List[str],
-  soil_spectra: np.array
-  ) -> None:
+def get_random_subgroups(df, n):
+    # Step 1: Shuffle the indices of the DataFrame
+    indices = np.arange(len(df))
+    np.random.shuffle(indices)
+    
+    # Step 2: Split the shuffled indices into `n` subgroups of roughly equal size
+    subgroups = np.array_split(indices, n)
+    
+    # Step 3: Create subgroups of the DataFrame using these indices
+    subgroups_dfs = [(indices, df.iloc[indices]) for indices in subgroups]
+    
+    return subgroups_dfs
 
-  logger = get_logger()
-  
-  # run PROSAIL forward runs for the different parametrizations available
-  logger.info('Starting PROSAIL runs')
-  lut_params_pheno = lut_params_dir.joinpath('prosail_danner-etal_switzerland.csv') # Only use general params for LUT
+def dataframe_to_lookup_table(df: pd.DataFrame, original_lut: LookupTable) -> LookupTable:
+    """
+    Converts a DataFrame to a LookupTable object.
 
-  pheno_phases = \
-      lut_params_pheno.name.split('.csv')[0] +'_S2A'
-      #lut_params_pheno.name.split('etal')[-1].split('.')[0][1::]
-
-  # generate lookup-table
-  trait_str = '-'.join(traits)
-  fpath_lut = output_dir.joinpath(
-    f'{pheno_phases}_{trait_str}_lut_no-constraints_gpr.pkl')
-
-  # Add soil spectra to inputs
-  # one method is to provide the spectra as rsoil0, but then it will not be scaled by the brightness and wetness params rsoil and psoil
-  # provide the spectra as soil_spectrum1 with soil_spectrum2 as zeros to utilise the sclaing
-  lut_inp = rtm_lut_config.copy()
-  #lut_inp['rsoil0'] = soil_spectra
-  lut_inp['soil_spectrum1'] = soil_spectra 
-  lut_inp['soil_spectrum2'] = np.zeros_like(soil_spectra) 
-  lut_inp['lut_params'] = lut_params_pheno
-  lut = generate_lut(**lut_inp) # TO DO: need to modify function so that it simulates for a range of angles
-
-  # special case CCC (Canopy Chlorophyll Content) ->
-  # this is not a direct RTM output
-  if 'ccc' in traits:
-    lut['ccc'] = lut['lai'] * lut['cab']
-    # convert to g m-2 as this is the more common unit
-    # ug -> g: factor 1e-6; cm2 -> m2: factor 1e-4
-    lut['ccc'] *= 1e-2
-
-  # prepare LUT for model training
-  # lut = lut[band_selection + traits].copy()
-  lut.dropna(inplace=True)
-
-  # If file already exists, append new simulations
-  if fpath_lut.exists():
-    # Load existing data
-    with open(fpath_lut, 'rb') as f:
-        existing_lut = pickle.load(f)
-
-    # Append new data to existing data
-    lut = pd.concat([existing_lut, lut], ignore_index=True)
-
-    # Save the combined LUT to file
-    with open(fpath_lut, 'wb') as f:
-        pickle.dump(lut, f, protocol=3)
-
-  if not fpath_lut.exists():
-    # save LUT to file
-    with open(fpath_lut, 'wb') as f:
-      pickle.dump(lut, f, protocol=3)
-
-  else:
-    pass
-
-  logger.info('Finished PROSAIL runs')
-
-
+    :param df: DataFrame to convert
+    :param original_lut: Original LookupTable object to copy parameters from
+    :return: LookupTable object
+    """
+    lut = LookupTable(original_lut._params_df)
+    lut.samples = df.reset_index(drop=True).copy()
+    return lut
 
 
 
@@ -180,23 +210,26 @@ if __name__ == '__main__':
   os.chdir(cwd)
 
   # global setup
-  out_dir = Path('../results').joinpath('lut_based_inversion/soil_scaled')
+  out_dir = Path('../results').joinpath('lut_based_inversion/test')
   out_dir.mkdir(exist_ok=True)
 
   # spectral response function of Sentinel-2 for resampling PROSAIL output
   fpath_srf = Path(
       '../data/auxiliary/S2-SRF_COPE-GSEG-EOPG-TN-15-0007_3.1.xlsx')
-  # RTM configurations for lookup-table generation
-  rtm_lut_config = {
-      'sensor': 'Sentinel2A',
-      'lut_size': 1000,
-      'fpath_srf': fpath_srf,
-      'remove_invalid_green_peaks': True,
+  # Configurations for lookup-table generation and RTM simulations
+  lut_config = {
+      'lut_size': 10000,
       'sampling_method': 'FRS',
-      'linearize_lai': False,
       'apply_glai_ccc_constraint': False,
       'apply_chlorophyll_carotiniod_constraint': False
   }
+  rtm_config = {
+    'sensor': 'Sentinel2B',
+    'fpath_srf': fpath_srf,
+    'remove_invalid_green_peaks': True,
+    'linearize_lai': False,
+  }
+
   # directory with LUT parameters for different phenological macro-stages
   lut_params_dir = Path('lut_params')
   # Path to soil spectra to use
@@ -213,7 +246,8 @@ if __name__ == '__main__':
         generate_spectra(
             output_dir=out_dir,
             lut_params_dir=lut_params_dir,
-            rtm_lut_config=rtm_lut_config,
+            lut_config=lut_config,
+            rtm_config=rtm_config,
             traits=traits
         )
 
@@ -225,26 +259,18 @@ if __name__ == '__main__':
   if soil_path is not None:
     # Loop over soil spectra
     soil_df = pd.read_pickle(soil_path)
-    n_samples_per_spectra = rtm_lut_config['lut_size']//len(soil_df) # number of samples to generate with one soil spectra
-    for i, row in soil_df.iterrows():
-      soil_spectra = row.values # convert to a np.array
-      rtm_lut_config['lut_size'] = n_samples_per_spectra
+    #n_samples_per_spectra = rtm_lut_config['lut_size']//len(soil_df) # number of samples to generate with one soil spectra
 
-      # Call generate_spectra with soil and n_samples_per_spectra
-      try:
-          generate_spectra_soil(
-              output_dir=out_dir,
-              lut_params_dir=lut_params_dir,
-              rtm_lut_config=rtm_lut_config,
-              traits=traits,
-              soil_spectra=soil_spectra
-          )
-      except Exception as e:
-          print(f'Error: {e}')
-          pass
+    try:
+        generate_spectra_soil(
+            output_dir=out_dir,
+            lut_params_dir=lut_params_dir,
+            lut_config=lut_config,
+            rtm_config=rtm_config,
+            traits=traits,
+            soil_df=soil_df
+        )
+    except Exception as e:
+        print(f'Error: {e}')
+        pass
 
-
-
-  
-
-  
